245f2d4de5f88e9df5b4eb56928c951530d011b0
Phuc Doan <kiugfractal161@gmail.com>
2012-12-21 01:29:33 +0200

    changing

--- public/myScript.js
+++ public/myScript.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var socket = io.connect('http://vinter-1601.herokuapp.com');
+	var socket = io.connect('http://vinter-1601.herokuapp.com/');
 
 	//get date 
 	function getDates(){
